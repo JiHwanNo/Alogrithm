6af36a31735fe7d429e7029403374285db30424e
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2022-01-22 16:15:03 +0900

    DFS

---
+++ Algorithm_Study/Algorithm_Study/Graph_Basic.cpp
@@ -0,0 +1,111 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//void CreateGraph_1()
+//{
+//	struct Vertex
+//	{
+//		vector<Vertex*> edges;
+//		// 정점을 연결해주는 선.
+//	};
+//
+//	vector<Vertex> v;
+//	v.resize(6); // 정점의 갯수는 6
+//
+//	v[0].edges.push_back(&v[1]);
+//	v[0].edges.push_back(&v[3]);
+//	v[1].edges.push_back(&v[0]);
+//	v[1].edges.push_back(&v[2]);
+//	v[1].edges.push_back(&v[3]);
+//	v[3].edges.push_back(&v[4]);
+//	v[5].edges.push_back(&v[4]);
+//
+//	// Q)  0 -> 3번 정점이 연결되어 있나요?
+//	bool connected = false;
+//	for (Vertex* edge : v[0].edges)
+//	{
+//		cout << edge << endl;
+//		if (edge == &v[3])
+//		{
+//			connected = true;
+//			break;
+//		}
+//	}
+//
+//}
+//
+//void CreateGraph_2()
+//{
+//
+//	vector<vector<int>> adjacent(6);
+//	adjacent[0] = { 1,3 };
+//	adjacent[1] = { 0,2,3 };
+//	adjacent[3] = { 4 };
+//	adjacent[5] = { 4 };
+//
+//	bool connected = false;
+//	for (int vertex : adjacent[0])
+//	{
+//		cout << vertex << endl;
+//		if (vertex == 3)
+//		{
+//			connected = true;
+//			break;
+//		}
+//	}
+//
+//	vector<int>& adj = adjacent[0];
+//	bool sonnected2 = (std::find(adj.begin(), adj.end(), 3) != adj.end());
+//}
+//
+//void CreateGraph_3()
+//{
+//	//읽는 방법 : adjacent[from][to]
+//	//행렬을 이용한 그래프 표현(2차원 배열)
+//	//메모리 소모가 심하지만, 빠른 접근이 가능하다.
+//	// 간선이 많은 경우 이점이 있다.
+//	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
+//	adjacent[0][1] = true;
+//	adjacent[0][3] = true;
+//	adjacent[1][0] = true;
+//	adjacent[1][2] = true;
+//	adjacent[1][3] = true;
+//	adjacent[3][4] = true;
+//	adjacent[5][4] = true;
+//
+//	bool connected = adjacent[0][3];
+//
+//	vector<vector<int>> adjacent2 =
+//	{
+//		vector<int>{-1,15,-1,35,-1,-1},
+//		vector<int>{15,-1,-+5,10,-1,-1},
+//		vector<int>{-1,-1,-1,-1,-1,-1},
+//		vector<int>{-1,-1,-1,-1,+5,-1},
+//		vector<int>{-1,-1,-1,-1,-1,-1},
+//		vector<int>{-1,-1,-1,-1,+5,-1},
+//	};
+//
+//}
+//int main()
+//{
+//	//CreateGraph_1(); 
+//	//CreateGraph_2();
+//
+//	int test[7][7] =
+//	{
+//	{ 0,1,2,3,4,5,6 },
+//	{ 1,1,2,3,4,5,6 },
+//	{ 1,2,3,3,4,5,6 },
+//	{ 1,2,3,4,4,5,6 },
+//	{ 1,2,3,4,5,6,6 },
+//	{ 1,2,3,4,5,6,7 },
+//	{ 9,2,3,4,5,6,7 }
+//	};
+//
+//	for (int te : test[6])
+//	{
+//		cout << te << endl;
+//	}
+//
+//}
--- Algorithm_Study/Algorithm_Study/main.cpp
+++ Algorithm_Study/Algorithm_Study/main.cpp
@@ -2,110 +2,95 @@
 #include <vector>
 using namespace std;
 
-void CreateGraph_1()
+// DFS (Depth First Search) 깊이 우선 탐색
+// 일단 갈 수 있는 방향0(깊이)을 가보고 탐색한다.
+// BFS (Breadth First Search) 너비 우선 탐색
+// 출발점에서 갈 수 있는 방향을 먼저 탐색 후 다음 방향을 설정한다.
+
+struct Vertex
 {
-	struct Vertex
+	//data
+};
+
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+vector<bool> visited;
+void CreateGraph()
+{
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6);
+	adjacent[0].push_back(1);
+	adjacent[0].push_back(3);
+	adjacent[1].push_back(0);
+	adjacent[1].push_back(2);
+	adjacent[1].push_back(3);
+	adjacent[3].push_back(4);
+	adjacent[5].push_back(4);
+}
+
+void Dfs(int here)
+{
+	visited[here] = true;
+	cout << "visited : " << here << endl;
+
+	//인접 리스트 version
+	//모든 인접 정점을 순회한다.
+	for (int i = 0; i < adjacent[here].size(); i++)
 	{
-		vector<Vertex*> edges;
-		// 정점을 연결해주는 선.
-	};
-
-	vector<Vertex> v;
-	v.resize(6); // 정점의 갯수는 6
-
-	v[0].edges.push_back(&v[1]);
-	v[0].edges.push_back(&v[3]);
-	v[1].edges.push_back(&v[0]);
-	v[1].edges.push_back(&v[2]);
-	v[1].edges.push_back(&v[3]);
-	v[3].edges.push_back(&v[4]);
-	v[5].edges.push_back(&v[4]);
-
-	// Q)  0 -> 3번 정점이 연결되어 있나요?
-	bool connected = false;
-	for (Vertex* edge : v[0].edges)
-	{
-		cout << edge << endl;
-		if (edge == &v[3])
+		int there = adjacent[here][i];
+		if (visited[there] == false)
 		{
-			connected = true;
-			break;
+			Dfs(there);
 		}
 	}
-
 }
-
-void CreateGraph_2()
+void CreateGraph2()
 {
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6);
 
-	vector<vector<int>> adjacent(6);
-	adjacent[0] = { 1,3 };
-	adjacent[1] = { 0,2,3 };
-	adjacent[3] = { 4 };
-	adjacent[5] = { 4 };
-
-	bool connected = false;
-	for (int vertex : adjacent[0])
+	adjacent = vector<vector<int>>
 	{
-		cout << vertex << endl;
-		if (vertex == 3)
-		{
-			connected = true;
-			break;
-		}
-	}
-
-	vector<int>& adj = adjacent[0];
-	bool sonnected2 = (std::find(adj.begin(), adj.end(), 3) != adj.end());
-}
-
-void CreateGraph_3()
-{
-	//읽는 방법 : adjacent[from][to]
-	//행렬을 이용한 그래프 표현(2차원 배열)
-	//메모리 소모가 심하지만, 빠른 접근이 가능하다.
-	// 간선이 많은 경우 이점이 있다.
-	vector<vector<bool>> adjacent(6, vector<bool>(6,false));
-	adjacent[0][1] = true;
-	adjacent[0][3] = true;
-	adjacent[1][0] = true;
-	adjacent[1][2] = true;
-	adjacent[1][3] = true;
-	adjacent[3][4] = true;
-	adjacent[5][4] = true;
-	
-	bool connected = adjacent[0][3];
-
-	vector<vector<int>> adjacent2 =
-	{
-		vector<int>{-1,15,-1,35,-1,-1},
-		vector<int>{15,-1,-+5,10,-1,-1},
-		vector<int>{-1,-1,-1,-1,-1,-1},
-		vector<int>{-1,-1,-1,-1,+5,-1},
-		vector<int>{-1,-1,-1,-1,-1,-1},
-		vector<int>{-1,-1,-1,-1,+5,-1},
+		{0,1,0,1,0,0},
+		{1,0,1,1,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,1,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,1,0},
 	};
-
 }
+//DFS
+
+void Dfs2(int here)
+{
+	visited[here] = true;
+	cout << "visited : " << here << endl;
+
+	for (int i = 0; i < adjacent[here].size(); i++)
+	{
+		if (adjacent[here][i] == 0)
+			continue;
+
+		if (visited[i] == false)
+			Dfs2(i);
+	}
+}
+
+void DfsAll()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		if (visited[i] == false)
+			Dfs(i);
+	}
+}
+
 int main()
 {
-	//CreateGraph_1(); 
-	//CreateGraph_2();
+	CreateGraph2();
 
-	int test[7][7] = 
-	{
-	{ 0,1,2,3,4,5,6 },
-	{ 1,1,2,3,4,5,6 },
-	{ 1,2,3,3,4,5,6 },
-	{ 1,2,3,4,4,5,6 },
-	{ 1,2,3,4,5,6,6 },
-	{ 1,2,3,4,5,6,7 },
-	{ 9,2,3,4,5,6,7 } 
-	};
+	visited = vector<bool>(6, false);
 
-	for (int te : test[6])
-	{
-		cout << te << endl;
-	}
-
+	Dfs2(0);
+	//DfsAll();
 }
