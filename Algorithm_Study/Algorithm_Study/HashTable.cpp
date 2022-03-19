9356248fd33c2f4d050ec487519f22a9a7ea416c
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2022-03-19 17:32:22 +0900

    DisjointSet

---
+++ Algorithm_Study/Algorithm_Study/HashTable.cpp
@@ -0,0 +1,133 @@
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//#include <stack>
+//#include <list>
+//using namespace std;
+//#include <thread>
+//// 해시 테이블
+//
+//// map vs hash_map
+//
+//// map : Red-Black Tree
+//// 균형적 이진 트리
+//// - 추가/탐색/삭제 O(logN)
+//
+//// C# dictionary = C++ map (X)
+//// C# dictionary = C++ unordered_map
+//
+//// hash_map(c++11 표준 unordered_map)
+//// - 추가/탐색/삭제 O(1)
+//
+//// 메모리를 내주고 속도를 얻는다.
+//// EX) 아파트 우편함
+//// [201][202][203][204][205]
+//// [101][102][103][104][105]
+//
+//// 1 ~ 1000 user( userId = 1 ~ 999 )
+//// [1][2][3] ... [999] check
+//
+//// 'hash', 'table'
+//// O(1)
+//void TestTable()
+//{
+//	struct User
+//	{
+//		int userID = 0;
+//		string username;
+//	};
+//
+//	vector<User> users;
+//	users.resize(1000);
+//
+//	users[777] = User{ 777, "Rookiss" };
+//
+//	string name = users[777].username;
+//	cout << name << endl;
+//
+//	// 테이블
+//	// 키를 알면, 데이터를 단번에 찾을 수 있다.
+//
+//	// 문제의 상황
+//	// int32_max (3억 ~)
+//	// 메모리는 무한이 아니다. => hash로 해결을 한다.
+//}
+//
+//// 보안
+//// EX) 
+////	id : rookiss + pw : qwer1234
+////	id : rookiss + pw : hash(qwer1234) -> asdlkfjas12341@2141%!#%!#@1asdfj
+////	DB [rookiss][asdlkfjas12341@2141%!#%!#@1asdfj]
+//// 비밀번호 찾기 -> 아이디 입력 / 폰 인증 -> 옛 ) 비밀번호는 ~ 현재) 새로운 비밀번호 ~
+//
+//void TestHash()
+//{
+//	struct User
+//	{
+//		int userID = 0; // 1~ int32_max
+//		string username;
+//	};
+//
+//	vector<User> users;
+//	users.resize(1000);
+//
+//	const int userID = 123456789;
+//	int key = (userID % 1000); // hash < 고유번호
+//
+//	users[key] = User{ userID, "Rookiss" };
+//
+//	User& user = users[key];
+//	if (user.userID == userID)
+//	{
+//		string name = user.username;
+//		cout << name << endl;
+//	}
+//
+//}
+//
+//// 충돌 문제 [key값이 동일한 상태]
+//// 123456789
+//// 789
+//
+//// 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾아나선다.
+//// - 선형 조사법 (linear probing)
+//// hash(key) + 1 -> hash(key) + 2
+//// - 이차 조사법 (quadratic probing)
+//// hash(key)+1^2 -> hash(key) + 2^2
+//// -etc
+//// 체이닝 - 동일한 값이 있다면 연결리스트를 이용하여 또하나의 배열을 만들어 연결해준다.
+//// 
+//
+//void TestHashTableChaining()
+//{
+//	struct User
+//	{
+//		int userID = 0; // 1~ int32_max
+//		string username;
+//	};
+//
+//	vector< vector<User>> users;
+//	users.resize(1000);
+//
+//	const int userID = 123456789;
+//	int key = (userID % 1000); // hash < 고유번호
+//
+//	users[key].push_back(User{ userID, "Rookiss" });
+//
+//	vector<User>& bucket = users[key];
+//	for (User& user : bucket)
+//	{
+//		if (user.userID == userID)
+//		{
+//			string name = user.username;
+//			cout << name << endl;
+//		}
+//	}
+//}
+//
+//
+//int main()
+//{
+//	TestTable();
+//
+//}
--- Algorithm_Study/Algorithm_Study/main.cpp
+++ Algorithm_Study/Algorithm_Study/main.cpp
@@ -1,133 +1,80 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <stack>
-#include <list>
 using namespace std;
-#include <thread>
-// 해시 테이블
 
-// map vs hash_map
+// 그래프 / 트리 응용
+// 오늘의 주제 : 최소 스패닝 트리 (Minimum Spanning Tree)
 
-// map : Red-Black Tree
-// 균형적 이진 트리
-// - 추가/탐색/삭제 O(logN)
+// 상호 배타적 집합 (Disjoint Set)
+// -> 유니온-파인드 Union - Find (합치기-찾기)
 
-// C# dictionary = C++ map (X)
-// C# dictionary = C++ unordered_map
 
-// hash_map(c++11 표준 unordered_map)
-// - 추가/탐색/삭제 O(1)
+// Lineage Battleground 개발
+// 혈맹전 + 서바이벌
+// 1인 팀 1000명 (팀 id 0~999)
+// 동맹 (1번팀 + 2번팀 = 1번팀)
 
-// 메모리를 내주고 속도를 얻는다.
-// EX) 아파트 우편함
-// [201][202][203][204][205]
-// [101][102][103][104][105]
 
-// 1 ~ 1000 user( userId = 1 ~ 999 )
-// [1][2][3] ... [999] check
-
-// 'hash', 'table'
-// O(1)
-void TestTable()
+// 트리 구조를 이용한 상호 배타적 집합의 표현
+// [0] [1] [2] [3] [4]
+struct Node
 {
-	struct User
-	{
-		int userID = 0;
-		string username;
-	};
+	Node* leader;
+};
 
-	vector<User> users;
-	users.resize(1000);
+// [1]			[3]
+// [2]		   [4][5]
+//				[0]
+// 시간복잡도 : 트리의 높이에 비례한 시간이 걸림.
+// 트리를 평평한 구조로 만들어야 효율이 증가한다. 한쪽으로 치우친 트리를 해결해야 한다.
+// -> 트리 높이를 비교하여 트리를 합칠떄, 조절한다.
+// (Union-By-Rank) 랭크에 의한 합치기 최적화
 
-	users[777] = User{ 777, "Rookiss" };
-
-	string name = users[777].username;
-	cout << name << endl;
-
-	// 테이블
-	// 키를 알면, 데이터를 단번에 찾을 수 있다.
-	
-	// 문제의 상황
-	// int32_max (3억 ~)
-	// 메모리는 무한이 아니다. => hash로 해결을 한다.
-}
-
-// 보안
-// EX) 
-//	id : rookiss + pw : qwer1234
-//	id : rookiss + pw : hash(qwer1234) -> asdlkfjas12341@2141%!#%!#@1asdfj
-//	DB [rookiss][asdlkfjas12341@2141%!#%!#@1asdfj]
-// 비밀번호 찾기 -> 아이디 입력 / 폰 인증 -> 옛 ) 비밀번호는 ~ 현재) 새로운 비밀번호 ~
-
-void TestHash()
+// 시간복잡도 O(Ackermann(n)) = O(1)
+class DisjointSet
 {
-	struct User
+public :
+	DisjointSet(int n) : _parent(n), _rank(n,1) // 트리의 높이
 	{
-		int userID = 0; // 1~ int32_max
-		string username;
-	};
-
-	vector<User> users;
-	users.resize(1000);
-
-	const int userID = 123456789;
-	int key = (userID % 1000); // hash < 고유번호
-
-	users[key] = User{ userID, "Rookiss" };
-
-	User& user = users[key];
-	if (user.userID == userID)
-	{
-		string name = user.username;
-		cout << name << endl;
-	}
-
-}
-
-	// 충돌 문제 [key값이 동일한 상태]
-	// 123456789
-	// 789
-
-	// 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾아나선다.
-	// - 선형 조사법 (linear probing)
-	// hash(key) + 1 -> hash(key) + 2
-	// - 이차 조사법 (quadratic probing)
-	// hash(key)+1^2 -> hash(key) + 2^2
-	// -etc
-	// 체이닝 - 동일한 값이 있다면 연결리스트를 이용하여 또하나의 배열을 만들어 연결해준다.
-	// 
-
-void TestHashTableChaining()
-{
-	struct User
-	{
-		int userID = 0; // 1~ int32_max
-		string username;
-	};
-
-	vector< vector<User>> users;
-	users.resize(1000);
-
-	const int userID = 123456789;
-	int key = (userID % 1000); // hash < 고유번호
-
-	users[key].push_back(User{ userID, "Rookiss" });
-
-	vector<User>& bucket = users[key];
-	for (User& user : bucket)
-	{
-		if (user.userID == userID)
+		for (int i = 0; i < n; i++)
 		{
-			string name = user.username;
-			cout << name << endl;
+			_parent[i] = i;
 		}
 	}
-}
+
+	int Find(int u)
+	{
+		if (u == _parent[u])
+			return u;
+
+		return _parent[u] = Find(_parent[u]);
+	}
+
+	int Merge(int u, int v)
+	{
+		u = Find(u);
+		v = Find(v);
+
+		if (u == v)
+			return;
+
+		if (_rank[u] > _rank[v])
+			swap(u, v);
+
+		_parent[u] = v;
+
+		if (_rank[u] == _rank[v])
+		{
+			_rank[v]++;
+		}
+	}
+private :
+	vector<int> _parent;
+	vector<int> _rank;
+};
+
 
 
 int main()
 {
-	TestTable();
-	
 }
